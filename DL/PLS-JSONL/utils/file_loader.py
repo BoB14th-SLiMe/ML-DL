6b78ad1617217a37c634cf2a11da2577ba389c44
dohyean <gkscjsehgus@naver.com>
2025-12-26 02:55:09 +0900

    json, jsonl 로드 파일 업로드

---
+++ DL/PLS-JSONL/utils/file_loader.py
@@ -0,0 +1,62 @@
+"""
+file_loader.py
+
+다음 파일을 로드하는 함수
+  - json
+  - jsonl
+
+input
+  - file_type : 파일 종류
+  - file_path : 파일 경로
+
+output
+  - 
+  
+"""
+
+from tqdm import tqdm
+import json
+from pathlib import Path
+
+from path_validation import path_validation
+
+def file_load(file_type: str, file_path: str):
+  try:
+    if(file_type == "json"):
+      return json_load(file_path)
+    elif(file_type == "jsonl"):
+      return jsonl_load(file_path)
+    else:
+      print("Error no file type (file_load)")
+  except Exception as e:
+    print(f"{e}")
+
+
+def json_load(file_path: str):
+  try:
+    path_validation(file_path)
+    return json.loads(Path(file_path).read_text())  
+  except Exception as e:
+    print(f"{e}")
+
+
+def jsonl_load(file_path: str):
+  try:
+    path_validation(file_path)
+    file_data = []
+    with open(file_path, "r", encoding="utf-8-sig") as fin:
+      for file_line in tqdm(fin, desc="jsonl load", leave=True, ncols=90):
+        file_line = file_line.strip()
+        if not file_line:
+          continue
+        try:
+          file_data.append(json.loads(file_line))
+        except json.JSONDecodeError:
+          print(f"Error json parsing : {file_line[:50]}")
+    return file_data
+  except Exception as e:
+    print(f"{e}")
+
+
+# result = file_load("jsonl", "./pattern_windows(기존 매핑).jsonl")
+# print(f"{result}")
