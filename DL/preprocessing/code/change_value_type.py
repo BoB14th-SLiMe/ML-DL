cc4f8f6f2612859a53fbf7da8a35158aee4bf270
dohyean <gkscjsehgus@naver.com>
2025-12-30 21:15:00 +0900

    hex 데이터 float, int 변환 함수 추가

--- DL/preprocessing/code/change_value_type.py
+++ DL/preprocessing/code/change_value_type.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*
-
 from typing import Any
 
-# 형변환
 def _to_int(value: Any) -> int:
     try:
         if value in (None, ""):
@@ -19,3 +17,9 @@ def _to_float(value: Any) -> float:
         return float(value)
     except Exception:
         return None
+
+def _hex_to_int(hex_str: str) -> int:
+    return int(hex_str, 16)
+
+def _hex_to_float(hex_str: str) -> float:
+    return float(int(hex_str, 16))
